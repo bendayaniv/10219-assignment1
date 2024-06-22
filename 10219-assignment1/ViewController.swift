66ab733c42ebf355e6cef90232125becb6714787
Yaniv Ben David <yanivd@wix.com>
2024-06-22 20:41:38 +0300

    Initial Commit

---
+++ 10219-assignment1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  10219-assignment1
+//
+//  Created by Yaniv Ben David on 22/06/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
