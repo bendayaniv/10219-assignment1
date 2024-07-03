4944cf6147792fcd583eb9049d4d87a01cce19f6
Yaniv Ben David <yanivd@wix.com>
2024-07-03 13:51:49 +0300

    adding another 2 view controllers, moving between all 3 (need to change the way)

---
+++ 10219-assignment1/EndScreen.swift
@@ -0,0 +1,24 @@
+//
+//  StartScreen.swift
+//  10219-assignment1
+//
+//  Created by Yaniv Ben David on 03/07/2024.
+//
+
+import UIKit
+
+class EndScreen: UIViewController {
+    
+//    @IBOutlet weak var startGameButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+//    @IBAction func moveToGame(_ sender: Any) {
+//        if let nextVC = storyboard?.instantiateViewController(withIdentifier: "ViewController") as? ViewController {
+//            navigationController?.pushViewController(nextVC, animated: true)
+//        }
+//    }
+    
+}
